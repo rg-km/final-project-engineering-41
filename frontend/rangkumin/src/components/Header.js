46599326bbe1c0266a480d744288d875909cc018
Via Listi Anggraeny <vialistiii30@gmail.com>
2022-06-09 23:46:22 +0700

    Add: Header

---
+++ frontend/rangkumin/src/components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import {Navbar, Nav, Container} from 'react-bootstrap'
+import logoWhite from '../assets/Logo-White.png'
+
+import '../styles/header.css'
+
+export default function Header() {
+  return (
+    <>
+        <Container fluid style={{ backgroundColor : "#13222C", }}>   
+        {/* NavBar */}
+            <Navbar expand="lg">
+                <Container>
+                    <div style={{width: "30%"}}>
+                        <Navbar.Brand href="#home" width="30%"><img src={logoWhite} alt="Logo image" width="180" height="auto" className="d-inline-block align-top"></img></Navbar.Brand>
+                    </div>
+                    <div>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className='text-end'>
+                                <Nav.Link href="#" className='px-3' style={{ color: "white" }}>Home</Nav.Link>
+                                <Nav.Link href="#" className='px-3' style={{ color: "white" }}>Course</Nav.Link>
+                                <Nav.Link href="#" className='px-3' style={{ color: "white" }}>About Us</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse> 
+                    </div>
+                    {/* kalo login berubah */}
+                    <div className="text-end" style={{width: "30%"}}>
+                        <button className="btn-Header ">Log In</button>
+                    </div>
+                </Container>
+            </Navbar>
+        </Container>
+    </>
+  )
+}
