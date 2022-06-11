636276e1bc1b51a0cd93c0912827afc8b322223a
Deaashari <deaashari10@gmail.com>
2022-06-11 14:51:52 +0700

    menambahkan api.go

--- backend/api/api.go
+++ backend/api/api.go
@@ -3,16 +3,30 @@ package api
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/rg-km/final-project-engineering-41.git/backend/repository"
 )
 
-func NewApi() {
+type API struct {
+	usersRepo   repository.UserRepository
+	materisRepo repository.MateriRepository
+	mux         *http.ServeMux
+}
+
+func NewApi(usersRepo repository.UserRepository, materisRepo repository.MateriRepository) {
 
 	mux := http.NewServeMux()
 	api := API{
-		usersRepo, //productsRepo
+		usersRepo, materisRepo, mux, //productsRepo
 	}
 
 	mux.Handle("/api/user/login", api.POST(http.HandleFunc(api.login)))
+	mux.Handle("/api/user/logout", api.POST(http.HandlerFunc(api.logout)))
+
+	//API with AuthMiddleware:
+	mux.Handle("/api/materis", api.GET(api.AuthMiddleWare(http.HandlerFunc(api.materiList))))
+
+	return
 
 }
 
@@ -21,6 +35,7 @@ func (api *API) Handler() *http.ServeMux {
 }
 
 func (api *API) Start() {
+	http.Handle("/", http.FileServer(http.Dir(".")))
 	fmt.Println("starting web server at http://localhost:8080/")
 	http.ListenAndServe(":8080", api.Handler())
 }
--- backend/api/dashboard.go
+++ backend/api/dashboard.go
@@ -1,5 +1,19 @@
 package api
 
-func dashboard() {
+type DashboardErrorResponse struct {
+	Error string `json:"error"`
+}
 
+type CartItem struct {
+	NamaMateri string `json:"nama_materi"`
+	Category   string `json:"category"`
+	Price      int    `json:"price"`
+	Quantity   int    `json:"quantity"`
+}
+
+type DashboardSuccessResponse struct {
+	Username     string     `json:"username"`
+	CartItems    []CartItem `json:"cart_items"`
+	TotalPrice   int        `json:"total_price"`
+	MoneyChanges int        `json:"money_changes"`
 }
