ff66abb066db954b3dbb984cf1f571852de381c6
grmdhn20 <72301067+grmdhn20@users.noreply.github.com>
2022-06-14 22:24:15 +0700

    Fix Width Form Bugs

--- frontend/rangkumin/src/components/RightSideRegister.js
+++ frontend/rangkumin/src/components/RightSideRegister.js
@@ -14,16 +14,17 @@ export class RightSideRegister extends Component {
         return (
       <body>
         <div className='side'>
-        <img className='ellipseReg' src={ellipseReg} alt="ellipseReg" />
-        <img className='vectorReg' src={vectorReg} alt="3D Model" />
-        <img className='logo-register' src={logo} alt="logo-register" />
+          <img className='ellipseReg' src={ellipseReg} alt="ellipseReg" />
+          <img className='vectorReg' src={vectorReg} alt="3D Model" />
+          <img className='logo-register' src={logo} alt="logo-register" />
+          </div>
         <h1 fontSize="4xl" className='text-signup'>Sign Up</h1>
         <h4 fontSize="4xl" className='text2-signup'><center>Please Enter Your Detail.</center></h4>
         {/* Dekstop Form */}
-        <Form className='form-register'>
+        <Form style={{width: "30%"}} className='form-register'>
         <Form.Group className="mb-3" controlId="formBasicName">
             <Form.Label>Nama</Form.Label>
-            <Form.Control style={{width: "150%"}} type="name" placeholder="Masukkan nama anda" />
+            <Form.Control type="name" placeholder="Masukkan nama anda" />
           </Form.Group>
           <Form.Label>Gender</Form.Label>
           <Form.Group className='mb-3' controlId='formBasicCheckbox'>
@@ -36,11 +37,11 @@ export class RightSideRegister extends Component {
                       <label className="form-check-label" for="inlineRadio2">Perempuan</label>
                     </div>
           </Form.Group>
-          <Form.Group  className="mb-3">
+          <Form.Group className="mb-3">
           <Form.Label>No. Telepon</Form.Label>
           <InputGroup>
           <InputGroup.Text style={{backgroundColor: "white"}} id="basic-addon1"><img src={phone}></img></InputGroup.Text>
-          <Form.Control type="text" placeholder="Masukkan nomor telepon" />
+          <Form.Control placeholder="Masukkan nomor telepon" />
           </InputGroup>
           </Form.Group>
       <Form.Group  className="mb-3">
@@ -58,7 +59,7 @@ export class RightSideRegister extends Component {
       </InputGroup>
       </Form.Group>
           <div className="d-grid gap-2 mb-3">
-          <Button style={{width: "150%",backgroundColor: "#FF725E", border: "none", color: "black", fontWeight: "bold"}} size='xl' type="submit">
+          <Button style={{backgroundColor: "#FF725E", border: "none", color: "white", fontWeight: "bold"}} size='xl' type="submit">
                      Login              
           </Button>
           </div>
@@ -92,7 +93,6 @@ export class RightSideRegister extends Component {
           Don’t have any account? <a className='sign-up-button' href='#'>Sign Up</a>
             </Form.Text>
         </Form> */}
-        </div>
       </body>
         )
     }
