a4b11fe5fd028ea5e1265cb568efd85455ef72a0
Deaashari <deaashari10@gmail.com>
2022-06-15 18:18:45 +0700

    Dea- Memperbarui database pada file main.go backend\db\migration\main.go

--- backend/db/migration/main.go
+++ backend/db/migration/main.go
@@ -1,60 +1,178 @@
 package main
 
+// Database untuk membuat table users, materi,
 import (
 	"database/sql"
 	"fmt"
 
-	//_ "github.com/go-sql-driver/mysql"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-type Data struct {
-	IdUser       string `db:"id_user"`
-	NamaMateri   string `db:"nama_materi"`
-	NamaKategori string `db:"nama_kategori"`
-	Tingkat      string `db:"tingkat"`
+type User struct {
+	ID       int    `db:"id"`
+	Username string `db:"username"`
+	Password string `db:"password"`
+	Fullname string `db:"fullname"`
+	TipeUser string `db:"tipe_user"`
 }
 
+type TipeUser struct {
+	ID             int    `db:"id"`
+	TipeUser       string `db:"tipe_user"`
+	KeteranganUser string `db:"keterangan_user"`
+}
+
+type Subject struct {
+	ID          int    `db:"id"`
+	IdSubject   string `db:"id_subject"`
+	NamaSubject string `db:"nama_subject"`
+}
+
+type Tingkat struct {
+	ID          int    `db:"id"`
+	IdTingkat   string `db:"id_movie"`
+	NamaTingkat string `db:"nama_tingkat"`
+}
+
+type Materi struct {
+	ID        int    `db:"id"`
+	IdMateri  string `db:"id_materi"`
+	IdSubject string `db:"id_subject"`
+	IdTingkat string `db:"id_movie"`
+	Tanggal   string `db:"tingkat"`
+	File      string `db:"file"`
+}
+
+// Migrate digunakan untuk melakukan migrasi database dengan data yang dibutuhkan
 func Migrate() (*sql.DB, error) {
 	db, err := sql.Open("sqlite3", "./rangkumin.db")
 	if err != nil {
 		panic(err)
 	}
-	sqlStmt := `CREATE TABLE IF NOT EXISTS materis(
-		id_user integer not null primary key AUTOINCREMENT,
-		nama_materi varchar(255) not null,
-		nama_kategori varchar(255) not null,
-		tingkat varchar(255) not null
-	);
-	CREATE TABLE IF NOT EXISTS users (
+
+	sqlStmt := `CREATE TABLE IF NOT EXISTS users (
 		id integer not null primary key AUTOINCREMENT,
-		username varchar(255) not null,
-		password varchar(255) not null,
-		role varchar(255) not null,
-		loggedin boolean not null
+		username VARCHAR(255),
+		password VARCHAR(255),
+		fullname VARCHAR(255),
+		tipe_user VARCHAR(255)	
 	);`
+
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(`INSERT INTO users (username, password, fullname, tipe_user)
+	VALUES
+	("deaash","0000","Dea Ashari", "AM"),
+	("ridwanamd","1111","Ahmad Ridwan", "FM"),
+	("gilangg","2222","Gilang Ramadhan", "BM"),
+	("daffasyam","3333","Asyam Daffa", "PM"),
+	("viaalis","4444","Via Alisti", "FM");`)
+	if err != nil {
+		panic(err)
+	}
+
+	sqlStmt = `CREATE TABLE IF NOT EXISTS tipe_user (
+		id integer not null primary key AUTOINCREMENT,
+		tipe_user VARCHAR(255),
+		keterangan_user VARCHAR(255)
+	);`
+
 	_, err = db.Exec(sqlStmt)
 	if err != nil {
 		return nil, err
 	}
 
 	_, err = db.Exec(`
-	INSERT INTO materis(nama_materi, nama_kategori, tingkat) VALUES
-    ('Bilangan Bulat', 'Matematika', 'SD'),
-    ('Hukum Newton', 'Fisika', 'SMP'),
-    ('Tenses', 'Bahasa Inggris', 'SMP'),
-    ('Indonesia pada Orde Lama', 'Sejarah', 'SMA')
-	
-	INSERT INTO users(username, password, role, loggedin) VALUES
-    ('aditira', '1234', 'admin', false),
-    ('dina', '4321', 'employee', false),
-    ('dito', '2552', 'employee', false);
-	`)
+	INSERT INTO 
+	tipe_user (tipe_user, keterangan_user)
+	VALUES
+	("AM", "Admin"),
+	("FM", "Free Member"),
+	("BM", "Basic Member"),
+	("PM", "Premium Member");`)
 
 	if err != nil {
-		fmt.Println(err)
-		fmt.Println("here")
+		panic(err)
 	}
+
+	sqlStmt = `CREATE TABLE IF NOT EXISTS subject (
+		id integer not null primary key AUTOINCREMENT,
+		id_subject VARCHAR(255),
+		nama_subject VARCHAR(255)
+	);`
+
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(`
+			INSERT INTO 
+			subject (id_subject, nama_subject)
+			VALUES 
+			    ("S01", "Matematika"),
+				("S02", "IPA"),
+				("S03", "IPS"),
+				("S03", "Bahasa Indonesia"),
+			    ("S02", "Bahasa Inggri");`)
+
+	if err != nil {
+		panic(err)
+	}
+
+	sqlStmt = `CREATE TABLE IF NOT EXISTS tingkat (
+		id integer not null primary key AUTOINCREMENT,
+		id_tingkat VARCHAR(255),
+		nama_tingkat VARCHAR(255)
+	);`
+
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(`
+			INSERT INTO 
+			tingkat (id_tingkat, nama_tingkat)
+			VALUES 
+			    ("T01", "SD"),
+				("T02", "SMP"),
+				("T03", "SMA");`)
+
+	if err != nil {
+		panic(err)
+	}
+
+	sqlStmt = `CREATE TABLE IF NOT EXISTS materi (
+		id integer not null primary key AUTOINCREMENT,
+		id_materi VARCHAR(255),
+		id_subject VARCHAR(255),
+		id_tingkat VARCHAR(255),
+		tanggal VARCHAR(255),
+		file VARCHAR(255)
+	);`
+
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(`
+			INSERT INTO 
+			materi (id_materi, id_subject, id_tingkat, tanggal, file)
+			VALUES 
+			    ("M001","S01", "TO1","22/02/2022","dddddddd"),
+				("M002","S02", "TO1","01/03/2022","aaaaaaaaaaa"),
+				("M003","S02", "TO2","24/02/2022","11111111111"),
+				("M004","S03", "TO2","22/03/2022","2222222222");`)
+
+	if err != nil {
+		panic(err)
+	}
+
 	return db, nil
 }
 
@@ -64,8 +182,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	rows, err := db.Query("SELECT * FROM materis")
+	// table 1
+	rows, err := db.Query("SELECT * FROM users")
 	if err != nil {
 		panic(err)
 	}
@@ -73,14 +191,72 @@ func main() {
 	defer rows.Close()
 
 	for rows.Next() {
-
-		var data1 Data
-
-		err = rows.Scan(&data1.IdUser, &data1.NamaMateri, &data1.NamaKategori, &data1.Tingkat)
+		var user User
+		err = rows.Scan(&user.ID, &user.Username, &user.Password, &user.Fullname, &user.TipeUser)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("%+v\n", data1)
+		fmt.Printf("%+v\n", user)
+	}
+
+	// table 2
+	rows, err = db.Query("SELECT * FROM tipe_user")
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		var tipeuser TipeUser
+		err = rows.Scan(&tipeuser.ID, &tipeuser.TipeUser, &tipeuser.KeteranganUser)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%+v\n", tipeuser)
+	}
+
+	// table 3
+	rows, err = db.Query("SELECT * FROM subject")
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		var subject Subject
+		err = rows.Scan(&subject.ID, &subject.IdSubject, &subject.NamaSubject)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%+v\n", subject)
+	}
+
+	// table 4
+	rows, err = db.Query("SELECT * FROM tingkat")
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		var tingkat Tingkat
+		err = rows.Scan(&tingkat.ID, &tingkat.IdTingkat, &tingkat.NamaTingkat)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%+v\n", tingkat)
+	}
+
+	// table 5
+	rows, err = db.Query("SELECT * FROM materi")
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		var materi Materi
+		err = rows.Scan(&materi.ID, &materi.IdMateri, &materi.IdSubject, &materi.IdTingkat, &materi.Tanggal, &materi.File)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%+v\n", materi)
 	}
 
 	defer rows.Close()
