903c71b15a382c11f172169883c37833390c7ddc
grmdhn20 <72301067+grmdhn20@users.noreply.github.com>
2022-06-16 11:02:33 +0700

    UI Paging for user's Account Profile Setting

--- frontend/rangkumin/src/App.js
+++ frontend/rangkumin/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import UserSetting from './components/user/userSetting';
 import Settings from './components/Settings';
 
 function App() {
   return (
     <>
       <Header />
+      <UserSetting />
       <Settings />
       <Footer />
     </>
--- frontend/rangkumin/src/components/Settings.js
+++ frontend/rangkumin/src/components/Settings.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Container, Card, CardGroup, Col, } from "react-bootstrap";
+import { Container, Form ,Button } from "react-bootstrap";
 import profilePicture from "../assets/settings/profilePicture.png";
+import vector3d from "../assets/settings/3dassets.png";
 import "../styles/settings.css"
 
 export default function Settings () {
@@ -10,6 +11,36 @@ export default function Settings () {
         <h5 style={{color: "gray"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h5>
         <h4 className="mt-5">Your Profile Picture</h4>
         <img style={{width: "154px"}} className="mt-4" src={profilePicture}></img>
+        <br></br>
+        <Button style={{width: "15%",backgroundColor: "#F55139", borderStyle: "none", borderRadius: "5px"}} className="mt-4">Change Profile Picture</Button>
+        <br></br>
+        <Button style={{width: "15%",backgroundColor: "white", borderColor: "#F55139", borderRadius: "5px", color: "#F55139"}} className="mt-3">Delete</Button>
+        <Form style={{marginTop: "50px"}}>
+        <Form.Group style={{width: "50%"}} className="mb-3" controlId="formBasicNama">
+            <Form.Label>Nama</Form.Label>
+            <Form.Control type="nama" placeholder="Via Listi Anggraeny" />
+        </Form.Group>
+        <Form.Group style={{width: "50%"}} className="mb-3">
+        <Form.Label>Gender</Form.Label>
+        <Form.Select>
+          <option>Perempuan</option>
+          <option>Laki - Laki</option>
+        </Form.Select>
+        </Form.Group>
+        <Form.Group style={{width: "50%"}} className="mb-3" controlId="formBasicNoTelp">
+            <Form.Label>Nomor Telepon</Form.Label>
+            <Form.Control type="notelp" placeholder="62 893 9201 29" />
+        </Form.Group>
+        <Form.Text className="text-muted">*Donec iaculis varius nunc vitae porta. Vestibulum nec sem quis dolor convallis.s</Form.Text>
+        <br></br>
+        <hr></hr>
+        <Button style={{width: "15%",backgroundColor: "#F55139", borderStyle: "none", borderRadius: "5px", marginLeft: "80%"}} className= "mt-2 mb-2" variant="primary" type="submit">
+            Simpan Perubahan
+        </Button>
+        <hr></hr>
+        </Form>
+        <img src={vector3d} className="mt-5" style={{width: "40%", position: "absolute", top: "70%", right: "5%"}}></img>
         </Container>
+        
     )
 }
