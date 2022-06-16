16a6733097b9f0dd92624c53e2748dfe0015d63f
Via Listi Anggraeny <vialistiii30@gmail.com>
2022-06-16 15:52:46 +0700

    Merge From Homepage Branch

--- frontend/rangkumin/src/App.js
+++ frontend/rangkumin/src/App.js
@@ -1,30 +1,9 @@
 import './App.css';
-<<<<<<< HEAD
 import Homepage from './components/Homepage';
 
 function App() {
   return (
     <Homepage />
-=======
-<<<<<<< HEAD
-import { LeftSideLogin } from './components/LeftSideLogin';
-=======
-import { RightSideRegister } from './components/RightSideRegister';
->>>>>>> ff66abb066db954b3dbb984cf1f571852de381c6
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-function App() {
-  return (
-    
-    <div>
-<<<<<<< HEAD
-      <LeftSideLogin />
-=======
-      <RightSideRegister />
->>>>>>> ff66abb066db954b3dbb984cf1f571852de381c6
-      </div>
-  
->>>>>>> 5302fdd6b01159b32af527365aff2561de40d9cd
   );
 }
 
