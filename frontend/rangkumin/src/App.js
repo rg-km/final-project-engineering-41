bd897bcd88a7a36130418a7f2fcf961da293881d
grmdhn20 <72301067+grmdhn20@users.noreply.github.com>
2022-06-16 16:25:18 +0700

    Merge Register Page

--- frontend/rangkumin/src/App.js
+++ frontend/rangkumin/src/App.js
@@ -1,20 +1,14 @@
 import './App.css';
-<<<<<<< HEAD
 import { LeftSideLogin } from './components/LeftSideLogin';
-=======
 import { RightSideRegister } from './components/RightSideRegister';
->>>>>>> ff66abb066db954b3dbb984cf1f571852de381c6
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     
     <div>
-<<<<<<< HEAD
-      <LeftSideLogin />
-=======
+      {/* <LeftSideLogin /> */}
       <RightSideRegister />
->>>>>>> ff66abb066db954b3dbb984cf1f571852de381c6
       </div>
   
   );
