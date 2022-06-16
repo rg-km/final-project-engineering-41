a79b8b031d6a2748d9e72c4a313c2f297de51f7b
grmdhn20 <72301067+grmdhn20@users.noreply.github.com>
2022-06-16 16:31:05 +0700

    Fix Typing Bug

--- frontend/rangkumin/src/App.js
+++ frontend/rangkumin/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import ForgotPass from './components/ForgotPass.js';
-import ResetPass from './components/ResetPass';
+import ResetPass from './components/resetPass';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
