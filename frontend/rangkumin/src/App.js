8c6ffa6df56c53e1348fd318982ec9cb301c6abc
grmdhn20 <72301067+grmdhn20@users.noreply.github.com>
2022-06-16 15:28:53 +0700

    Paging UI for Subscription Setting on User's Subscription Setting

--- frontend/rangkumin/src/App.js
+++ frontend/rangkumin/src/App.js
@@ -10,8 +10,8 @@ function App() {
   return (
     <>
       <Header />
-      {/* <AccountSettings />
-      <PasswordSettings /> */}
+      {/* <AccountSettings /> */}
+      {/* <PasswordSettings /> */}
       <SubscriptionSettings />
       <Footer />
     </>
