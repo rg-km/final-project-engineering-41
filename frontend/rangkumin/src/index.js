2a7fd500071894f96df16b6ca6e659d3dde932ad
Asyamdaff <sasyam011@gmail.com>
2022-06-16 19:24:06 +0700

    Update hero.css

--- frontend/rangkumin/src/index.js
+++ frontend/rangkumin/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import "./styles/hero.css";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
