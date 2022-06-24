b5d54c22ee76ebff517eef7bd3102a83883a0df2
Deaashari <deaashari10@gmail.com>
2022-06-24 22:27:49 +0700

    menambah api user by role

--- backend/repository/users.go
+++ backend/repository/users.go
@@ -93,3 +93,26 @@ func (u *UserRepository) FetchUserRole(username string) (*string, error) {
 
 	return &user.Role, nil
 }
+func (u *UserRepository) FetchUserByRole(Role string) ([]User, error) {
+	// TODO: You must implement this function for fetch product by name
+	//beginanswer
+	//var materi Materi
+	rows, err := u.db.Query("SELECT * FROM users WHERE role = ?", Role)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var users []User
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.ID, &user.Username, &user.Email, &user.Password, &user.NoTelp, &user.Role, &user.Loggedin)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+	return users, nil
+
+	//return materi, nil
+	//endanswer return Product{}, nil
+}
